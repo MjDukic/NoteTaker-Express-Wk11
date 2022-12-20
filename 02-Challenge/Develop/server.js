0727eb7533639388ebe045a026f31ff635991050
MJ <mirjanadukicc@gmail.com>
2022-12-19 17:57:37 -0800

    added route

--- 02-Challenge/Develop/server.js
+++ 02-Challenge/Develop/server.js
@@ -1,5 +1,7 @@
 //for heroku deployment
 const express = require('express');
+const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -7,6 +9,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
+
+app.use("/api/notes", apiRoutes);
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+}) 
 
 
 //spin up server
