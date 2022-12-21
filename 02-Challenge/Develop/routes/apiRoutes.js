48c96fd7d620c15eb42990d0fecfce9111cec86d
MJ <mirjanadukicc@gmail.com>
2022-12-21 12:14:23 -0800

    working on get request

--- 02-Challenge/Develop/routes/apiRoutes.js
+++ 02-Challenge/Develop/routes/apiRoutes.js
@@ -1,13 +1,18 @@
 const router = require("express").Router();
+// const fs = require('fs');
+// const uuid = require('../helpers/uuid');
 
 router.get('/', (req, res) => {
     //get all the notes from db.json
-    res.json(`GOT YOUR ${req.method} REQUEST`);
+    console.info(`GOT YOUR ${req.method} REQUEST`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    
 });
 
 router.post('/', (req, res) => {
     // add a note to the DB
-    res.json(`GOT YOUR ${req.method} REQUEST`);
+    console.info(`GOT YOUR ${req.method} REQUEST`);
+
 });
 
 module.exports = router;
