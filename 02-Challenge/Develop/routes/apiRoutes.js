9eac3446d5aefc31a13d7962485e0683a88df869
MJ <mirjanadukicc@gmail.com>
2022-12-21 12:52:20 -0800

    added helpers. working on get and post requests, 500 error showing

--- 02-Challenge/Develop/routes/apiRoutes.js
+++ 02-Challenge/Develop/routes/apiRoutes.js
@@ -1,17 +1,39 @@
 const router = require("express").Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 // const fs = require('fs');
 // const uuid = require('../helpers/uuid');
 
 router.get('/', (req, res) => {
     //get all the notes from db.json
-    console.info(`GOT YOUR ${req.method} REQUEST`);
+    console.info(`RECIEVED YOUR ${req.method} REQUEST`);
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
     
 });
 
 router.post('/', (req, res) => {
     // add a note to the DB
-    console.info(`GOT YOUR ${req.method} REQUEST`);
+    console.info(`YOUR ${req.method} REQUEST WORKED`);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
+        
+        readAndAppend(newNote, './db/db.json');
+
+        const response = {
+            status: 'note is saved',
+            body: newNote,
+        };
+
+        res.json(response);
+    } else {
+      res.json('Note not saved');
+    }
 
 });
 
